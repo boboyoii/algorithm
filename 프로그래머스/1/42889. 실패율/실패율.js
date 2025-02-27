fe563b1715678356b090cdcd9b6ab3416772bbbc
boyeon <139124147+bobo-403@users.noreply.github.com>
2025-02-27 15:42:33 +0900

    [level 1] Title: 실패율, Time: 9170.61 ms, Memory: 67.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,22 +1,10 @@
 function solution(N, stages) {
-    let answer = [];
-    let user = [];
-    for(let i=0; i<=N+1; i++)
-        user[i] = 0;
-    
-    for(let i=0; i<stages.length; i++)
-        user[stages[i]]++;
-    
-    let fRate = {};
+    let fRate = [];
     for(let i=1; i<=N; i++){
-        let challenger = 0, success = 0;
-        for(let j=i; j<=N+1; j++){
-            if( j != i ) success += user[j];
-            challenger += user[j];
-        }
-        fRate[i] = success / challenger;
+        const reach = stages.filter((v) => v >= i).length;
+        const curr = stages.filter((v) => v > i).length;
+        fRate.push([i, curr/reach]);
     }
-    let sorted = Object.entries(fRate).sort((a, b) => a[1] - b[1]);
-    answer = sorted.map((v) => Number(v[0]));
-    return answer;
+    fRate.sort((a,b) => a[1] - b[1]);
+    return fRate.map((v) => v[0]);
 }
