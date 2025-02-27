ef07564467a64c4f51d45c8c664c71f9496b4164
boyeon <139124147+bobo-403@users.noreply.github.com>
2025-02-27 14:41:31 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.63 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,26 +1,20 @@
 function solution(today, terms, privacies) {
     let destroyNum = [];
     const periods = {};
+    const [year, month, date] = today.split('.').map(Number);
+    const todates = year * 12 * 28 + month * 28 + date;
     
     for(let term of terms){
         const [type, period] = term.split(' ');
         periods[type] = Number(period);
     }
     
-    for(let i = 0; i<privacies.length; i++){
-        const [day, type] = privacies[i].split(' ');
-        let [year, month, date] = day.split('.').map(Number);
-        date -= 1;
-        month += periods[type];
-        if(date === 0){
-            date = 28;
-            month -= 1;
-        }
-        year += (Math.floor((month-1)/12));
-        month = month % 12 || 12;
-        const deadLine = `${String(year)}.${String(month).padStart(2, '0')}.${String(date).padStart(2, '0')}`;
-        if(deadLine < today)
-            destroyNum.push(i+1);
-    }
+    privacies.forEach((v, i) => {
+        const [day, type] = v.split(' ');
+        const date = day.split('.').map(Number);
+        const dates = date[0] * 12 * 28 + date[1] * 28 + date[2] + periods[type] * 28;
+        if(todates >= dates) destroyNum.push(i+1);
+    });
+    
     return destroyNum;
 }
