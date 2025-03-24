5c3fedfa1e3db07b17d923d44842144333e4631c
boyeon <139124147+bobo-403@users.noreply.github.com>
2025-03-24 20:41:00 +0900

    [level 1] Title: 햄버거 만들기, Time: 129.66 ms, Memory: 96.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,16 @@
+const BURGER = [1,2,3,1];
+const equals = (a, b) => a.length === b.length && a.every((v, i) => v === b[i]);
+
+function solution(ingredient) {
+    let count = 0;
+    let stack = [];
+    
+    ingredient.forEach((item) => {
+        stack.push(item);
+        if(equals(stack.slice(-4), BURGER)){
+            stack.splice(stack.length-4,4);
+            count += 1;
+        }
+    })
+    return count;
+}
