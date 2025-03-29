065a636f1e748fb39238e98bb6dcba879fedcfe8
boyeon <139124147+bobo-403@users.noreply.github.com>
2025-03-29 15:12:23 +0900

    [level 1] Title: 과일 장수, Time: 256.03 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,10 @@
+function solution(k, m, score) {
+    let result = 0
+    score.sort((a,b) => b-a);
+    
+    for(let i=0; i <= score.length - m; i += m){
+        result += (score[i+(m-1)] * m);
+    }
+    
+    return result;
+}
