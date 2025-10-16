ead06661b35ad2c1a2aeedb7083506fb1ca16047
Boyeon <139124147+boboyoii@users.noreply.github.com>
2025-10-16 16:15:46 +0900

    [Silver III] Title: 계단 오르기, Time: 96 ms, Memory: 9368 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.js
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.js
@@ -1,23 +1,27 @@
-const input = require('fs')
-  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
-  .toString()
-  .trim()
-  .split('\n')
-  .map(Number);
+function solution(input) {
+  const N = input[0];
+  const stairs = input.slice(1);
+  const D = Array.from({ length: N }, (x) => [0, 0]);
 
-const step = input[0];
-let stairs = input.slice(1);
-stairs.unshift(0);
-const score = Array.from({ length: step + 1 }, () => 0);
+  if (N === 1) return stairs[0];
+  D[0][0] = stairs[0];
+  D[0][1] = stairs[0];
+  D[1][0] = stairs[1];
+  D[1][1] = D[0][0] + stairs[1];
 
-score[1] = stairs[1];
-score[2] = stairs[1] + stairs[2];
+  for (let i = 2; i < N; i++) {
+    D[i][0] = Math.max(D[i - 2][0], D[i - 2][1]) + stairs[i];
+    D[i][1] = D[i - 1][0] + stairs[i];
+  }
 
-for (let i = 3; i < score.length; i++) {
-  score[i] = Math.max(
-    score[i - 2] + stairs[i],
-    score[i - 3] + stairs[i - 1] + stairs[i]
-  );
+  return Math.max(D[N - 1][0], D[N - 1][1]);
 }
 
-console.log(score[step]);
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+console.log(solution(input));
