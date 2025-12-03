329dde38ee5d79500c57244c8643fc6487b7c10d
Boyeon <139124147+boboyoii@users.noreply.github.com>
2025-12-03 11:48:20 +0900

    [Silver I] Title: 미로 탐색, Time: 164 ms, Memory: 11484 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.js
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -1,32 +1,9 @@
-function solution(input) {
-  const maze = input.slice(1).map((v) => v.split(''));
-  const visit = Array.from({ length: maze.length }, () =>
-    Array.from({ length: maze[0].length }, () => false)
-  );
-
-  const queue = [];
-  queue.push({ pos: [0, 0], extent: 1 });
-
-  while (queue.length !== 0) {
-    const can = queue.shift();
-    const [x, y] = can.pos;
-
-    if (x === maze.length - 1 && y === maze[0].length - 1) {
-      return can.extent;
-    }
-    if (visit[x][y]) continue;
-    visit[x][y] = true;
-
-    if (x - 1 > -1 && !visit[x - 1][y] && maze[x - 1][y] === '1')
-      queue.push({ pos: [x - 1, y], extent: can.extent + 1 });
-    if (x + 1 < maze.length && !visit[x + 1][y] && maze[x + 1][y] === '1')
-      queue.push({ pos: [x + 1, y], extent: can.extent + 1 });
-    if (y - 1 > -1 && !visit[x][y - 1] && maze[x][y - 1] === '1')
-      queue.push({ pos: [x, y - 1], extent: can.extent + 1 });
-    if (y + 1 < maze[0].length && !visit[x][y + 1] && maze[x][y + 1] === '1')
-      queue.push({ pos: [x, y + 1], extent: can.extent + 1 });
-  }
-}
+const dir = [
+  [-1, 0],
+  [1, 0],
+  [0, 1],
+  [0, -1],
+];
 
 const input = require('fs')
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
@@ -34,4 +11,31 @@ const input = require('fs')
   .trim()
   .split('\n');
 
-console.log(solution(input));
+const [N, M] = input[0].split(' ').map(Number);
+const mazeMap = input.slice(1).map((line) => line.split(''));
+
+function solution(N, M, mazeMap) {
+  const dist = Array.from({ length: N }, () => Array(M).fill(-1));
+  const queue = [];
+  queue.push([0, 0]);
+  dist[0][0] = 1;
+
+  while (queue.length !== 0) {
+    const [x, y] = queue.shift();
+    const currentDist = dist[x][y];
+
+    for (let i = 0; i < 4; i++) {
+      const nx = x + dir[i][0];
+      const ny = y + dir[i][1];
+      if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+      if (mazeMap[nx][ny] === '0') continue;
+      if (dist[nx][ny] !== -1) continue;
+      queue.push([nx, ny]);
+      dist[nx][ny] = currentDist + 1;
+    }
+  }
+
+  return dist[N - 1][M - 1];
+}
+
+console.log(solution(N, M, mazeMap));
