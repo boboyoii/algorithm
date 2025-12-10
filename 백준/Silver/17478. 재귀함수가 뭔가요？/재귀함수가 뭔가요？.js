b65275aad26a2247f00d1ea4900bc5246a9798c1
Boyeon <139124147+boboyoii@users.noreply.github.com>
2025-12-10 13:22:39 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 100 ms, Memory: 9684 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.js
@@ -0,0 +1,31 @@
+const input = +require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim();
+
+const total = +input;
+
+console.log('어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.');
+
+function recursion(N) {
+  const header = '____'.repeat(total - N);
+  console.log(`${header}"재귀함수가 뭔가요?"`);
+  if (N === 0) {
+    console.log(`${header}"재귀함수는 자기 자신을 호출하는 함수라네"`);
+    console.log(`${header}라고 답변하였지.`);
+    return;
+  }
+  console.log(
+    `${header}"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.`
+  );
+  console.log(
+    `${header}마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.`
+  );
+  console.log(
+    `${header}그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."`
+  );
+  recursion(N - 1);
+  console.log(`${header}라고 답변하였지.`);
+}
+
+recursion(total);
