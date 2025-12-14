6b2a16876f276bb4d89babad27d308a27ea5caa1
Boyeon <139124147+boboyoii@users.noreply.github.com>
2025-12-14 22:19:47 +0900

    [Silver III] Title: N과 M (7), Time: 788 ms, Memory: 211568 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.js
@@ -0,0 +1,29 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+const [N, M] = input[0].split(' ').map(Number);
+const arr = input[1]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
+const result = [];
+const temp = [];
+
+function solution(k, idx) {
+  if (k === M) {
+    result.push(temp.join(' '));
+    return;
+  }
+
+  for (let i = 0; i < N; i++) {
+    temp.push(arr[i]);
+    solution(k + 1, i + 1);
+    temp.pop();
+  }
+}
+
+solution(0, 0);
+console.log(result.join('\n'));
